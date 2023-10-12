42238546fc8a265f8a8582bccc18a9779fe853be
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2023-10-12 13:25:54 +0300

    Delete lab1 directory

--- lab1/ex4.py
+++
@@ -1,9 +0,0 @@
-# Write a script that converts a string of characters written in UpperCamelCase into lowercase_with_underscores.
-import re
-
-def camelcase_to_snakecase(input_string):
-    snakecase_string = re.sub(r'([a-z])([A-Z])', r'\1_\2', input_string)
-    return snakecase_string.lower()
-
-input_string = input("Enter a string in UpperCamelCase: ")
-print(camelcase_to_snakecase(input_string))
